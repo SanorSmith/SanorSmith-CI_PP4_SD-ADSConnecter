f4c575b1bba8905e7657af3552853f4b42529f0c
SanorSmith <141268934+SanorSmith@users.noreply.github.com>
2024-09-20 22:34:57 +0000

    Create a sign_in view in ads/views.py

--- servicetisement/ads/views.py
+++ servicetisement/ads/views.py
@@ -2,7 +2,8 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import login, authenticate
 from django.contrib import messages
 from .forms import CustomUserCreationForm
-
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.models import User
 # Create your views here.
 
 def home(request):
@@ -26,3 +27,24 @@ def register(request):
     else:
         form = CustomUserCreationForm()
     return render(request, 'register.html', {'form': form})
+
+def sign_in(request):
+    if request.method == 'POST':
+        username_or_email = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(username=username_or_email, password=password)
+        if user is None:
+            try:
+                user = User.objects.get(email=username_or_email)
+                user = authenticate(username=user.username, password=password)
+            except User.DoesNotExist:
+                pass
+
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+        else:
+            messages.error(request, 'Invalid username/email or password.')
+
+    form = AuthenticationForm()
+    return render(request, 'sign_in.html', {'form': form})
