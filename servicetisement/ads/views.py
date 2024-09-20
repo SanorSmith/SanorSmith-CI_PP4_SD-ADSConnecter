e22d9c43314c17ce412770e402c59afd8a3cefe9
SanorSmith <141268934+SanorSmith@users.noreply.github.com>
2024-09-20 22:26:19 +0000

    Create register.html in templates

--- servicetisement/ads/views.py
+++ servicetisement/ads/views.py
@@ -1,6 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, authenticate
+from django.contrib import messages
+from .forms import CustomUserCreationForm
 
 # Create your views here.
 
 def home(request):
     return render(request, 'home.html')
+
+def register(request):
+    if request.method == 'POST':
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            messages.success(request, 'Registration successful.')
+            return redirect('home')
+        else:
+            for field in form.errors:
+                for error in form.errors[field]:
+                    messages.error(request, error)
+    else:
+        form = CustomUserCreationForm()
+    return render(request, 'register.html', {'form': form})
