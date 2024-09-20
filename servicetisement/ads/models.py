4dfd346b16d2b23cb08a196c964c732c6b2b871d
SanorSmith <141268934+SanorSmith@users.noreply.github.com>
2024-09-20 22:18:28 +0000

    Create UserProfile and Service models

--- servicetisement/ads/models.py
+++ servicetisement/ads/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
 # Create your models here.
+from django.db import models
+from django.contrib.auth.models import User
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    bio = models.TextField(blank=True)
+    contact_info = models.CharField(max_length=255, blank=True)
+
+    def __str__(self):
+        return self.user.username
+
+class Service(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    occupation = models.CharField(max_length=100)
+    contact_info = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
