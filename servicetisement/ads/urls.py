f1da3c79490840609801245506f2bc37309c470a
SanorSmith <141268934+SanorSmith@users.noreply.github.com>
2024-09-21 19:35:29 +0000

    adding and viewing services

--- servicetisement/ads/urls.py
+++ servicetisement/ads/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
     path('register/', views.register, name='register'),
     path('sign-in/', views.sign_in, name='sign_in'),
     path('sign-out/', views.sign_out, name='sign_out'),
+    path('available-services/', views.available_services, name='available_services'),
+    path('add-service/', views.add_service, name='add_service'),
 ]
