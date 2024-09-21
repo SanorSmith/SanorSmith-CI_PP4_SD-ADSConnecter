3bf109fdcf48494420248096bbf843ba4ca6e3f3
SanorSmith <141268934+SanorSmith@users.noreply.github.com>
2024-09-21 19:23:10 +0000

    Add service categories to the admin panel

--- servicetisement/ads/admin.py
+++ servicetisement/ads/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import UserProfile, Service
+from .models import UserProfile, Service, ServiceCategory
 
 admin.site.register(UserProfile)
 admin.site.register(Service)
+admin.site.register(ServiceCategory)
