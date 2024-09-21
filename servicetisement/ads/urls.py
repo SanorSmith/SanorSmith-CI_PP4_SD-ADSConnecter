46f0bb9b7ab195224a0dc5afd3e286f02fa060d8
SanorSmith <141268934+SanorSmith@users.noreply.github.com>
2024-09-21 20:43:28 +0000

    Connect edit_servic.html and remove_service.html to views

--- servicetisement/ads/urls.py
+++ servicetisement/ads/urls.py
@@ -8,4 +8,6 @@ urlpatterns = [
     path('sign-out/', views.sign_out, name='sign_out'),
     path('available-services/', views.available_services, name='available_services'),
     path('add-service/', views.add_service, name='add_service'),
+    path('update-service/<int:service_id>/', views.update_service, name='update_service'),
+    path('remove-service/<int:service_id>/', views.remove_service, name='remove_service')
 ]
