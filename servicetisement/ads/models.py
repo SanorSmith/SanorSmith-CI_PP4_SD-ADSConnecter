77785289912fb8b1b0af44e7f9172631a1c5471e
SanorSmith <141268934+SanorSmith@users.noreply.github.com>
2024-09-21 19:10:42 +0000

    Create ServiceCategory Model

--- servicetisement/ads/models.py
+++ servicetisement/ads/models.py
@@ -22,3 +22,8 @@ class Service(models.Model):
 
     def __str__(self):
         return self.title
+class ServiceCategory(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
