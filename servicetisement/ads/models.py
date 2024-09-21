7876a3ffea8f35ad540cfdd3d69d4b82e2cabce1
SanorSmith <141268934+SanorSmith@users.noreply.github.com>
2024-09-21 19:21:27 +0000

    Update the database and add cloudinaryfiled model and Migrate the changes

--- servicetisement/ads/models.py
+++ servicetisement/ads/models.py
@@ -1,8 +1,7 @@
 from django.db import models
-
-# Create your models here.
 from django.db import models
 from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
 
 class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
@@ -13,10 +12,12 @@ class UserProfile(models.Model):
         return self.user.username
 
 class Service(models.Model):
+    ads_author = models.ForeignKey(User, on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
     description = models.TextField()
     occupation = models.CharField(max_length=100)
     contact_info = models.CharField(max_length=255)
+    featured_image = CloudinaryField('image', default='placeholder')
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
