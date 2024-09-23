a5deac42de736d47397098ade9deeb8593faef19
SanorSmith <141268934+SanorSmith@users.noreply.github.com>
2024-09-23 20:38:21 +0000

    Update and replace env.py file

---
+++ servicetisement/env.py
@@ -0,0 +1,9 @@
+import os
+os.environ["DATABASE_URL"]="postgres://uv8pf9joage:Uv4IFCUy0Jou@ep-gentle-mountain-a23bxz6h.eu-central-1.aws.neon.tech/shape_slush_untie_194152?sslmode=require&options=endpoint%3Dep-gentle-mountain-a23bxz6h"
+os.environ["SECRET_KEY"]="my_key2024@key"
+os.environ["CLOUDINARY_URL"]="cloudinary://746713743875148:TKyiil7pnpSaIUpxwqb3Rm6g6vQ@dxawpgmpj"
+
+# these variables for Cloudinary configuration
+os.environ["CLOUDINARY_CLOUD_NAME"] = "dxawpgmpj"
+os.environ["CLOUDINARY_API_KEY"] = "746713743875148"
+os.environ["CLOUDINARY_API_SECRET"] = "TKyiil7pnpSaIUpxwqb3Rm6g6vQ"
