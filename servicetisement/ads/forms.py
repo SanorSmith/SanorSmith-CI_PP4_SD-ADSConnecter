6693f00a11af3e12f9773606bed61da153036355
SanorSmith <141268934+SanorSmith@users.noreply.github.com>
2024-09-25 20:16:02 +0000

    Update and restyle the CustomUserCreationForm to include necessary fields and widgets

--- servicetisement/ads/forms.py
+++ servicetisement/ads/forms.py
@@ -6,6 +6,24 @@ from .models import Service, ServiceCategory
 class CustomUserCreationForm(UserCreationForm):
     email = forms.EmailField(required=True)
 
+class CustomUserCreationForm(UserCreationForm):
+    email = forms.EmailField(required=True, widget=forms.EmailInput(attrs={
+        'class': 'form-control',
+        'placeholder': 'Enter your email'
+    }))
+    username = forms.CharField(widget=forms.TextInput(attrs={
+        'class': 'form-control',
+        'placeholder': 'Enter a username'
+    }))
+    password1 = forms.CharField(label="Password", widget=forms.PasswordInput(attrs={
+        'class': 'form-control',
+        'placeholder': 'Enter a password'
+    }))
+    password2 = forms.CharField(label="Confirm Password", widget=forms.PasswordInput(attrs={
+        'class': 'form-control',
+        'placeholder': 'Confirm your password'
+    }))
+
     class Meta:
         model = User
         fields = ('username', 'email', 'password1', 'password2')
